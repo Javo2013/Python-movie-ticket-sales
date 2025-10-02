b1a64058e081dd1d176952f5589a35a91f65e0f0
Javian Williams <javian.williams29@gmail.com>
2025-10-02 10:41:31 -0400

    debugging question 3 responses

---
+++ question3.py
@@ -0,0 +1,25 @@
+# question3.py
+
+#input 
+pet_type = input("enter pet type (dog/cat/bird/hamster):")
+human_age = int(input("enter your pet's age in human years:"))
+
+#conversion
+if pet_type == 'dog:' or pet_type == 'cat':
+  if human_age <=2:
+  pet_age = human_age * 12 # 2 years = 24
+else:
+  pet_age = 24 + (human_age - 2) * 4
+elif pet_type == "bird":
+pet_age = human_age * 9
+elif pet_type =="hamster":
+pet_age = human_age * 25
+else:
+print("sorry, unsupported pet type.")
+
+#print result 
+print("\n===Pet Age Conversion ===")
+print(f"pet Type: {pet_type.capitalize}")
+print(f"human age:{human_age} years")
+print(f"pet age: {pet_age} pet years")
+print(f"/n A fun fact: your {pet_type} is like a {pet_age}-year-old human!")
